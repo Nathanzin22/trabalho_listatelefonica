0494e4e721d470409e4d28ecb215bc154ab2a9b9
nathanzin13 <111472970+nathanzin13@users.noreply.github.com>
2022-09-26 15:50:57 -0300

    Fim do postgree

--- app/src/test/java/db/DBHelper.java
+++ app/src/test/java/db/DBHelper.java
@@ -1,4 +1,29 @@
-package db;
+package br.unigran.trabalho_listatelefonica.db;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(@Nullable Context context) {
+        super(context, "ListaTelefonica", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(
+                "create table listaTelefonica(" +
+                        "id integer primary key autoincrement," +
+                        "nome varchar(150)," +
+                        "data_nascimento varchar(10)," +
+                        "telefone varchar(15))"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
