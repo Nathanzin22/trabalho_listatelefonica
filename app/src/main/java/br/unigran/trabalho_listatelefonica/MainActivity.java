a6a67a3fd0126681dab9be31f5672a3bb5de199a
nathanzin13 <111472970+nathanzin13@users.noreply.github.com>
2022-09-26 15:55:39 -0300

    Inicio das funcionalidades

--- app/src/main/java/br/unigran/trabalho_listatelefonica/MainActivity.java
+++ app/src/main/java/br/unigran/trabalho_listatelefonica/MainActivity.java
@@ -2,13 +2,24 @@ package br.unigran.trabalho_listatelefonica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.unigran.trabalho_listatelefonica.db.DBHelper;
+import br.unigran.trabalho_listatelefonica.db.TelefoneDB;
+import br.unigran.trabalho_listatelefonica.entidades.Telefone;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
