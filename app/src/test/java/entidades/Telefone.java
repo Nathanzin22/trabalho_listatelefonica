53ea3aa2a03a0feb950d481a2eca0a21e8340412
nathanzin13 <111472970+nathanzin13@users.noreply.github.com>
2022-09-26 15:46:07 -0300

    Fim das entidades

--- app/src/test/java/entidades/Telefone.java
+++ app/src/test/java/entidades/Telefone.java
@@ -5,3 +5,40 @@ public class Telefone {
     private String nome;
     private String telefone;
     private String data_nascimento;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String toString() {
+        return (String.format("Nome: %s / Telefone: %s / Data de Nascimento: %s", nome, telefone, data_nascimento));
+    }
+}
