72214f88d223db9e692ea30395018298c220b87b
nathanzin13 <111472970+nathanzin13@users.noreply.github.com>
2022-09-26 15:45:13 -0300

    Criação das entidades

---
+++ app/src/test/java/entidades/Telefone.java
@@ -0,0 +1,7 @@
+package br.unigran.trabalho_listatelefonica.entidades;
+
+public class Telefone {
+    private Integer id;
+    private String nome;
+    private String telefone;
+    private String data_nascimento;
